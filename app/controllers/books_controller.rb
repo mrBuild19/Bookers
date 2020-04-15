416cce00219398740d5c2bb9caadf7220480c861
阿武大輔 <daisuke@MacBook-Air.local>
2020-04-15 11:34:45 +0900

    [Add]投稿編集画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,10 +8,6 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
   end
 
-  def new
-  	# @book = Book.new
-  end
-
   def create
   	book = Book.new(book_params)
   	book.save
@@ -19,6 +15,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book.id)
+
   end
 
   private
