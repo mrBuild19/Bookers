3a3ec41ec5d996e7689435b1ecd8389a8815b231
阿武大輔 <daisuke@MacBook-Air.local>
2020-04-15 19:10:08 +0900

    [Add]バリデーションチェック、エラーメッセージ、サクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,14 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book)
+  	@book = Book.new(book_params)
+  	@books = Book.all
+  	if @book.save
+  		flash[:notice] = 'Book was successfully created.'
+  		redirect_to @book
+  	else
+  		render action: :index
+  	end
   end
 
   def edit
@@ -19,9 +24,13 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book.id)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		flash[:notice] = 'Book was successfully created.'
+  		redirect_to book_path(@book.id)
+  	else
+  		render action: :edit
+  	end
   end
 
   def destroy
